4be37c7dd9b52a24a4390dadafa244f795b14dd1
Kullmann <seankullmann@gmail.com>
2020-11-08 15:06:22 -0500

    Fixed shell script

--- decrypt.py
+++ decrypt.py
@@ -1,11 +1,11 @@
 # Need top get key with from encrypt function, and then decrypt
 # with RSA first, and then AES key
-from Crypto.Cipher import AES
-key = "letsgetthisbread".encode("utf8")
-cipher_d = AES.new(key, AES.MODE_EAX)
+#from Crypto.Cipher import AES
+#key = "letsgetthisbread".encode("utf8")
+#cipher_d = AES.new(key, AES.MODE_EAX)
 
 def decrypt(image):
-    image_trimmed = image[64:-2]
-    plaintext = cipher_d.decrypt(image_trimmed)
-    plaintext = image[0:64] + plaintext + image[-2:]
+  #  image_trimmed = image[64:-2]
+  #  plaintext = cipher_d.decrypt(image_trimmed)
+  #  plaintext = image[0:64] + plaintext + image[-2:]
     return image
