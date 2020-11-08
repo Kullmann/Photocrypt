a9a1623f7768c415e37c8c91ccd321e119bac501
Kullmann <seankullmann@gmail.com>
2020-11-08 16:17:55 -0500

    Got image to display from server

--- photo-rsa/src/screens/Main/index.js
+++ photo-rsa/src/screens/Main/index.js
@@ -6,14 +6,13 @@
 import React, { Component } from "react";
 import GithubLogo from "../../components/GithubLogo";
 import Logo from "../../components/Logo";
-import ActivityIndicator from "../../components/ActivityIndicator"
-import * as axios from 'axios';
+import ActivityIndicator from "../../components/ActivityIndicator";
+import * as axios from "axios";
 
-export default class Main extends Component{
-  
+export default class Main extends Component {
   constructor() {
     super();
-    this.state = {loading:false, image:""}
+    this.state = { loading: false, image: "", eImage: false };
     this.imageInputRef = React.createRef();
   }
 
@@ -22,69 +21,90 @@ export default class Main extends Component{
 
     reader.onloadend = () => {
       const base64 = reader.result;
-      if(base64) {
-        this.setState({image:base64.toString()});
+      if (base64) {
+        this.setState({ image: base64.toString() });
         //console.log(base64.toString());
         this.makeServerRequest();
       }
-    }
+    };
     if (event.target.files[0]) {
       reader.readAsDataURL(event.target.files[0]);
     }
-    this.setState({loading:true});
-  }
+    this.setState({ loading: true });
+  };
 
-  makeServerRequest = async() => {
+  makeServerRequest = async () => {
     try {
       const res = await axios.post("/encrypt", this.state.image);
-      const str = Buffer.from(res.data, 'binary').toString('base64');
+      const str = Buffer.from(res.data, "binary"); //.toString("base64");
       console.log(str);
-      this.setState({loading:false, image:""})
+      this.setState({ loading: false, image: str, eImage: true });
       this.state.document = true;
     } catch (err) {
       console.log("error: " + err);
     }
-  }
+  };
 
   render() {
-      return (
-        <div className="Container">
-          <div className="Header">
-            <div className="Title">
-              <Logo style={{marginRight:15}}/>
-              <div class="TitleContent">
-                <p className="TitleText">Encrypt Your Photo!</p>
-                <p className="SubtitleText">Created by Sean Kullmann and Hosung Lee</p>
-              </div>
-              </div>
-            <div className="Description">
-              <p>This Web Based App encrypts your photo using RSA</p>
-              <p>and draws an image that represents your encrypted photo data.</p>
+    return (
+      <div className="Container">
+        <div className="Header">
+          <div className="Title">
+            <Logo style={{ marginRight: 15 }} />
+            <div class="TitleContent">
+              <p className="TitleText">Encrypt Your Photo!</p>
+              <p className="SubtitleText">
+                Created by Sean Kullmann and Hosung Lee
+              </p>
+              {this.state.eImage ? (
+                <img src={"data:image/bmp;base64," + this.state.image}></img>
+              ) : (
+                <p>Please upload an image!</p>
+              )}
             </div>
           </div>
-          <div className="Content">
-            <div>
-              <p className="Message">Give it a try! Press the Button below</p>
-              {
-                !this.state.loading ?
-                <form className="UploadForm">
-                  <label class="UploadButton">
-                    <p>Upload</p>
-                    <input id="imageInput" type="file" onChange={this.onChangeImage.bind(this)} ref={this.imageInputRef}/>
-                  </label>
-                  <div className="Option">
-                    <input type="checkbox"></input>
-                    <a>I want to decrypt my cypherphoto.</a>
-                  </div>
-                </form> 
-                : <div className="UploadForm"><ActivityIndicator/></div>
-              }
-              </div>
-              <div className="Footer">
-                <p><a href="https://github.com/Kullmann/RSAPhotoCryptography">Check out our github!</a></p><a href="https://github.com/Kullmann/RSAPhotoCryptography"><GithubLogo style={{marginLeft: 10}}/></a>
-              </div>
+          <div className="Description">
+            <p>This Web Based App encrypts your photo using RSA</p>
+            <p>and draws an image that represents your encrypted photo data.</p>
           </div>
         </div>
+        <div className="Content">
+          <div>
+            <p className="Message">Give it a try! Press the Button below</p>
+            {!this.state.loading ? (
+              <form className="UploadForm">
+                <label class="UploadButton">
+                  <p>Upload</p>
+                  <input
+                    id="imageInput"
+                    type="file"
+                    onChange={this.onChangeImage.bind(this)}
+                    ref={this.imageInputRef}
+                  />
+                </label>
+                <div className="Option">
+                  <input type="checkbox"></input>
+                  <a>I want to decrypt my cypherphoto.</a>
+                </div>
+              </form>
+            ) : (
+              <div className="UploadForm">
+                <ActivityIndicator />
+              </div>
+            )}
+          </div>
+          <div className="Footer">
+            <p>
+              <a href="https://github.com/Kullmann/RSAPhotoCryptography">
+                Check out our github!
+              </a>
+            </p>
+            <a href="https://github.com/Kullmann/RSAPhotoCryptography">
+              <GithubLogo style={{ marginLeft: 10 }} />
+            </a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
