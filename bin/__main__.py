1e54087da4b4adaa70582f6731db582c8d313ac3
Hosung Lee <runway3237@gmail.com>
2020-12-07 07:44:40 +0900

    finished cui

--- bin/__main__.py
+++ bin/__main__.py
@@ -4,7 +4,11 @@
 @description: Main program
 '''
 
+import sys
 import argparse
+from os.path import splitext
+from photocrypt import open_image, encrypt_image, decrypt_image
+from photocrypt.crypto.RSA import load_key
 
 if __name__ == "__main__":
 
@@ -13,15 +17,66 @@ if __name__ == "__main__":
         )
 
     parser.add_argument(
-        '-e',
-        metavar='test image path',
-        help='testing image on given path'
+        '-i',
+        metavar='image path',
+        help='image file path to encrypt/decrypt'
         )
 
     parser.add_argument(
-        '-d',
-        metavar='model path',
-        help='set classifier model specified on given path'
+        '-k',
+        metavar='key path',
+        help='public key path'
         )
 
+    parser.add_argument(
+        '--encrypt',
+        action='store_const',
+        const=True,
+        help='encrypt given image using provided key'
+        )
+
+    parser.add_argument(
+        '--decrypt',
+        action='store_const',
+        const=True,
+        help='decrypt given image using provided key'
+        )
+
+    use_cui = False
+    encryption = None
+
     args = parser.parse_args()
+    if args.encrypt and args.decrypt:
+        print('can only perform encryption or decryption at once.')
+        sys.exit()
+
+    if args.encrypt or args.decrypt:
+        if not args.i:
+            print('image is required for encryption/decryption.')
+            sys.exit()
+        if not args.k:
+            print('key is required for encryption/decryption.')
+            sys.exit()
+
+    if args.i and args.k:
+        if not args.encrypt and not args.decrypt:
+            print('use --encryption or --decryption to specify which to perform.')
+
+        else:
+            use_cui = True
+            encryption = 'encrypt' if args.encrypt else 'decrypt'
+    
+    if use_cui:
+        image = open_image(args.i)
+        image_path, image_ext = splitext(args.i)
+        key = load_key(args.k)
+        if encryption == 'encrypt':
+            encrypted = encrypt_image(image, key)
+            encrypted.save(f'{image_path}_enc{image_ext}')
+            print(f'image encrypted in {image_path}_enc{image_ext}')
+        else:
+            decrypted = decrypt_image(image, key)
+            decrypted.save(f'{image_path}_dec{image_ext}')
+            print(f'image decrypted in {image_path}_dec{image_ext}')
+    else:
+        pass
