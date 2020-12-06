20e333e9aa3297e6eb2897f75c7b0a398810fb04
Hosung Lee <runway3237@gmail.com>
2020-12-07 07:06:42 +0900

    added tests

--- tests/test_photocrypt.py
+++ tests/test_photocrypt.py
@@ -1,44 +1,42 @@
 """
 Tests photocrypt module for image encryption and decryption
 """
+
+import unittest
 from photocrypt import encrypt_image, open_image, decrypt_image
-from photocrypt.crypto.RSA import generate_key
+from photocrypt.crypto.RSA import generate_key, save_keypair, load_key
 
-if __name__ == '__main__':
-    n_pass = 0
-    n_fail = 0
-
-    print("generating RSA key...")
-    try:
+class TestKeyManager(unittest.TestCase):
+    """
+    Tests image encryption and decryption
+    """
+    def test_rsa(self):
+        """
+        Tests RSA
+        """
         private, public = generate_key()
-        print("pass")
-        n_pass += 1
-        
-    except Exception as e:
-        print("fail")
-        print(e)
-        n_fail += 1
-    print("encrypting image...")
-    try:
+        self.assertEqual(isinstance(private, bytes), True)
+        self.assertEqual(isinstance(public, bytes), True)
+        save_keypair((private, public), ("private.pem","public.pem"))
+
+
+    def test_encryption(self):
+        """
+        Tests image encryption
+        """
+        public = load_key("public.pem")
         eimg = encrypt_image(open_image("samples/tuatara.jpg"), public)
         eimg.save("samples/tuatara_ecb.jpg")
-        print("pass")
-        n_pass += 1
 
-    except Exception as e:
-        print("fail")
-        print(e)
-        n_fail += 1
-    try:
-        print("decrypting image...")
-        eimg = decrypt_image(open_image("samples/tuatara_ecb.jpg"), private)
-        eimg.save("samples/tuatara_dcb.jpg")
-        print("pass")
-        n_pass += 1
+    def test_decryption(self):
+        """
+        Tests image decryption
+        """
+        private = load_key("private.pem")
+        dimg = decrypt_image(open_image("samples/tuatara_ecb.jpg"), private)
+        with self.assertRaises(ValueError):
+            decrypt_image(open_image("samples/tuatara.jpg"), private)
+        dimg.save("samples/tuatara_dcb.jpg")
 
-    except Exception as e:
-        print("fail")
-        print(e)
-        n_fail += 1
-
-    print(f"passed: {n_pass} / {n_fail + n_pass}")
+if __name__ == '__main__':
+    unittest.main()
